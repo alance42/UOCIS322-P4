6d088e6f831c2e9191460a7728f57b3cc3b46be8
Avi Lance <alance@uoregon.edu>
2021-05-15 19:14:21 -0700

    Finished program testing suite and acp_times.py

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -12,33 +12,62 @@ import arrow
 #  these signatures even if you don't use all the
 #  same arguments.
 #
+speedData = [[0, 15, 34], [200, 15, 32 ], [400, 15, 30], [600, 11.428, 28]]
 
+def calculate(control_dist_km, brevet_dist_km, open):
+	timeDiff = 0
+	pastDistance = 0
+	index = 2 if open else 1
+	
+	if control_dist_km < 0 or control_dist_km > 1000:
+		return 0
+
+	if control_dist_km == 0:
+		return 60
+
+	for i in range(len(speedData)-1, -1, -1):
+		if control_dist_km > speedData[i][0]:
+			speed = speedData[i][index]
+			tempDist = control_dist_km - pastDistance - speedData[i][0]
+			pastDistance += tempDist
+			timeDiff += (tempDist / speed) * 60
+			
+	return round(timeDiff)
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
-    """
-    Args:
-       control_dist_km:  number, control distance in kilometers
-       brevet_dist_km: number, nominal distance of the brevet
-           in kilometers, which must be one of 200, 300, 400, 600,
-           or 1000 (the only official ACP brevet distances)
-       brevet_start_time:  A date object (arrow)
-    Returns:
-       A date object indicating the control open time.
-       This will be in the same time zone as the brevet start time.
-    """
-    return arrow.now()
+	"""
+	Args:
+	   control_dist_km:  number, control distance in kilometers
+	   brevet_dist_km: number, nominal distance of the brevet
+		   in kilometers, which must be one of 200, 300, 400, 600,
+		   or 1000 (the only official ACP brevet distances)
+	   brevet_start_time:  A date object (arrow)
+	Returns:
+	   A date object indicating the control open time.
+	   This will be in the same time zone as the brevet start time.
+	"""
+	time = calculate(control_dist_km, brevet_dist_km, True)
+	brevet_start_time = brevet_start_time.shift(minutes=(time % 60))
+	brevet_start_time = brevet_start_time.shift(hours=(time // 60))
+	brevet_start_time.to('local').to('utc')
+	return brevet_start_time
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
-    """
-    Args:
-       control_dist_km:  number, control distance in kilometers
-          brevet_dist_km: number, nominal distance of the brevet
-          in kilometers, which must be one of 200, 300, 400, 600, or 1000
-          (the only official ACP brevet distances)
-       brevet_start_time:  A date object (arrow)
-    Returns:
-       A date object indicating the control close time.
-       This will be in the same time zone as the brevet start time.
-    """
-    return arrow.now()
+	"""
+	Args:
+	   control_dist_km:  number, control distance in kilometers
+		  brevet_dist_km: number, nominal distance of the brevet
+		  in kilometers, which must be one of 200, 300, 400, 600, or 1000
+		  (the only official ACP brevet distances)
+	   brevet_start_time:  A date object (arrow)
+	Returns:
+	   A date object indicating the control close time.
+	   This will be in the same time zone as the brevet start time.
+	"""
+
+	time = calculate(control_dist_km, brevet_dist_km, False)
+	brevet_start_time = brevet_start_time.shift(minutes=(time % 60))
+	brevet_start_time = brevet_start_time.shift(hours=(time // 60))
+	brevet_start_time.to('local').to('utc')
+	return brevet_start_time
